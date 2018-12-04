ad2776e0e8129820ecddbf53cf393baf6e65628c
Matt Davis <m.davis@certentinc.com>
2018-12-04 15:19:01 -0800

    completed tutorial part 7

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -3,30 +3,17 @@
 (function() {
     angular.module("phoneList")
         .component("phoneList", {
-            controller: phoneListComponent,
+            controller: ["$http", phoneListComponent],
             templateUrl: "phone-list/phone-list.template.html"
         });
 
-    function phoneListComponent() {
-        const me = this;
+    function phoneListComponent($http) {
         console.debug("phoneList component controller function");
-        me.phones = [
-            {
-                age: 1,
-                name: "Nexus S",
-                snippet: "Fast just got faster with Nexus S."
-            },
-            {
-                age: 2,
-                name: "Motorola XOOM™ with Wi-Fi",
-                snippet: "The Next, Next Generation tablet."
-            },
-            {
-                age: 3,
-                name: "MOTOROLA XOOM™",
-                snippet: "The Next, Next Generation tablet."
-            }
-        ];
-        me.orderProp = "age";
+        const self = this;
+        self.orderProp = "age";
+
+        $http.get("phones/phones.json").then(function(response) {
+            self.phones = response.data;
+        });
     }
 })();
